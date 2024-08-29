e5fb4f88b72dc1d10539a8c51ab9bd0fff4e596e
Will McElderry <wmcelderry@users.noreply.github.com>
2024-08-29 11:44:48 +0100

    Create a primary/central key that is used to encrypt all content.

--- scripts/note.sh
+++ scripts/note.sh
@@ -21,7 +21,7 @@ label="$1"
 shift
 value="$*"
 
-notebook_file="${base_dir}/data/my_notebook"
+notebook_file="${base_dir}/data/my_primary_notebook"
 
 
 
@@ -30,7 +30,7 @@ function dummy()
     #ensure any matching label is deleted.
     if [[ -e "${notebook_file}" ]] ; then
         sed -i "/^${label}::/d" "${notebook_file}"
-    else 
+    else
         #put the KDF salt in the notebook_file.
         openssl rand -hex 16 > "${notebook_file}"
     fi
@@ -41,21 +41,87 @@ function dummy()
     dummy_entry "${salt}" "${label}" "${value}" >> "${notebook_file}"
 }
 
+
+function create_notebook_file()
+{
+    local file="${1}"
+    local primary_key_hex="$(openssl rand -hex 32)"
+
+    set_password "${primary_key_hex}" >> "${file}"
+
+    echo "${primary_key_hex}"
+}
+
+function change_password()
+{
+    local file="${notebook_file}"
+
+    echo "Enter existing password to unlock the key material:"
+    local primary_key_hex="$(retrieve_pkey "${file}")"
+
+    if [[ -z "${primary_key_hex}" ]] ; then
+        echo "Failed to unlock the key."
+        return
+    fi
+
+    echo "Enter new password for future operation:"
+    local password_line="$(set_password "${primary_key_hex}")"
+
+    # confirm the password to ensure it can be unlocked!
+    echo "Re-enter the new password to confirm correct spelling!"
+    local conf_pkey="$(retrieve_pkey_from_line "${password_line}")"
+
+    if [[ "${conf_pkey,,*}" != "${primary_key_hex,,*}" ]] ; then
+        echo "New passwords do not match!"
+    else
+        sed -i "1s!.*!${password_line}!g" "${file}"
+    fi
+}
+
+
+function set_password()
+{
+    local primary_key_hex="$1"
+
+    #put the KDF salt and primary key in the notebook_file.
+    salt="$(openssl rand -hex 16)"
+
+    echo  -n "${salt}"
+    #store the primary key, protected by the primary password
+
+    #encrypt the primary key with AES:
+    encrypt "${salt}" "${primary_key_hex}"
+
+    #echo "${primary_key_hex}"
+}
+
+function delete_entry()
+{
+    local label notebook_file
+
+    label="$1"
+    notebook_file="$2"
+
+    sed -i "/^${label}::/d" "${notebook_file}"
+}
+
 function store()
 {
     local salt
-    #ensure any matching label is deleted.
+
     if [[ -e "${notebook_file}" ]] ; then
-        sed -i "/^${label}::/d" "${notebook_file}"
-    else 
-        #put the KDF salt in the notebook_file.
-        openssl rand -hex 16 > "${notebook_file}"
+        delete_entry "${label}" "${notebook_file}"
+        primary_key_hex="$(retrieve_pkey "${notebook_file}")"
+        if [[ -z "${primary_key_hex}" ]] ; then
+            echo "Incorrect key for encrypting."
+            return
+        fi
+    else
+        primary_key_hex="$(create_notebook_file "${notebook_file}")"
     fi
 
-    salt="$(retrieve_salt "${notebook_file}")"
-
-    #append the new value
-    encrypt_entry "${salt}" "${label}" "${value}" >> "${notebook_file}"
+    #echo encrypt_entry_with_key "${label}" "${primary_key_hex}" "${value}"
+    encrypt_entry_with_key "${label}" "${primary_key_hex}" "${value}" >> "${notebook_file}"
 }
 
 
@@ -70,12 +136,17 @@ function get_entry()
 function retrieve()
 {
     val="$(get_entry "${label}")"
-    salt="$(retrieve_salt "${notebook_file}")"
+    primary_key_hex="$(retrieve_pkey "${notebook_file}")"
+
+    if [[ -z "${primary_key_hex}" ]] ; then
+        echo "Incorrect key for decrypting."
+        return
+    fi
 
     if [[ -z "${val}" ]] ; then
         echo "Cannot find entry for '${label}'"
     else
-        decrypt_entry "${salt}" "${val}"
+        decrypt_entry_with_key "${primary_key_hex}" "${val}"
     fi
 }
 
@@ -85,13 +156,13 @@ function getkey()
 
     salt="$1"
 
-    echo "Enter password now:" >&2
+    echo "Enter password:" >&2
     read  -s pass
 
     openssl kdf \
         -keylen 32 \
         -kdfopt "pass:${pass}" \
-        -kdfopt salt:"${salt}" \
+        -kdfopt "salt:${salt}" \
         -kdfopt n:1024 \
         -kdfopt r:8 \
         -kdfopt p:16 \
@@ -114,16 +185,56 @@ function encrypt()
     shift
     value_hex="$1"
 
+    #echo "Salt: ${salt}" >&2
 
-    local key iv data hmac
-    key="$(getkey "${salt}")"
+    local key="$(getkey "${salt}")"
+
+    encrypt_using_key "${key}" "${value_hex}"
+}
+
+function mk_one_line()
+{
+    sed -n 'H;${x;s/\n//g;p}'
+}
+
+function encrypt_using_key()
+{
+
+    local key="$1"
+    shift
+    local value_hex="$1"
+
+    local iv data hmac
 
 
+    tmp="${#key}"
+    if [[ "${tmp}" -ne 64 ]]; then
+        echo "Key too short, length ${#key}" >&2
+        return
+    fi
+    #echo "KEY: ${key}" >&2
     iv="$(openssl rand -hex 16 )"
-    data="$(xxd -r -p <<< "${value_hex}" | openssl enc -a -e -aes-256-ctr -nopad -K "${key}" -iv "${iv}"  | sed -n 'H;${x;s/\n//g;p}')"
+    #echo "IV: ${iv}" >&2
+    data="$(xxd -r -p <<< "${value_hex}" | openssl enc -a -e -aes-256-ctr -nopad -K "${key}" -iv "${iv}" | mk_one_line)"
+    #echo "data: ${data}" >&2
     hmac="$(openssl mac -digest SHA256 -macopt "hexkey:${key}" HMAC <<< "${data}")"
+    #echo "MAC: ${hmac}" >&2
 
-    echo "${iv}${hmac}${data}"
+    echo "$(xxd -r -p <<< "${iv}${hmac}" | base64 | mk_one_line)${data}"
+}
+
+function encrypt_entry_with_key()
+{
+    local label primary_key_hex value_hex
+    label="$1"
+    shift
+    primary_key_hex="$1"
+    shift
+    value_hex="$(xxd -p <<< "${*}")"
+
+    cipher_text=$(encrypt_using_key "${primary_key_hex}"  "${value_hex}")
+
+    echo "${label}::${cipher_text}"
 }
 
 function encrypt_entry()
@@ -156,51 +267,105 @@ function dummy_entry()
     echo "${label}::${iv}${hmac}${data}"
 }
 
-function decrypt_entry()
+
+
+function decrypt_using_key()
 {
-
-    salt="$1"
+    local key="$1"
     shift
-    data="$1"
+    local data="$*"
 
-    key="$(getkey "${salt}")"
-
-    #trim the label:
-    label="${data/::*/}"
-    data="${data/*::/}"
+    data="$(base64 -d <<< "${data:0:64}" | xxd -p | mk_one_line)${data:64}"
 
     #parse the data into constituents:
-    iv="${data:0:32}"
-    data_hmac="${data:32:64}"
-    data="${data:96}"
+    local iv="${data:0:32}"
+    local data_hmac="${data:32:64}"
+    local data="${data:96}"
 
-    hmac="$(openssl mac -digest SHA256 -macopt "hexkey:${key}" HMAC <<< "${data}")"
+    local hmac="$(openssl mac -digest SHA256 -macopt "hexkey:${key}" HMAC <<< "${data}")"
 
-    if [[ "${hmac}"  != "${data_hmac}" ]]; then
-        echo "Invalid key provided for this entry."
-    else
+    if [[ "${hmac,,*}"  == "${data_hmac}" ]]; then
+        #echo "Invalid key provided for this entry."
+    #else
         openssl enc -d -aes-256-ctr -nopad -a  -K "${key}" -iv "${iv}" <<< "${data}"
     fi
 }
 
-function retrieve_salt()
+function decrypt_entry_with_key()
 {
-    file="$1"
-    #head -n 1 "${file}"
-    sed -n '1{p;q}' "${file}"
+    local key="$1"
+    shift
+    local entry="$1"
+
+
+    #trim the label:
+    local label="${entry/::*/}"
+    local cipher_text="${entry/*::/}"
+
+    plain_text="$(decrypt_using_key "${key}" "${cipher_text}")"
+
+    if [[ -z "${plain_text}" ]]; then
+        echo "Invalid key provided for this entry."
+    else
+        echo "${plain_text}"
+    fi
 }
 
-case ${mode} in 
+function decrypt_entry()
+{
+
+    local salt="$1"
+    shift
+    local entry="$1"
+
+    local key="$(getkey "${salt}")"
+
+    #trim the label:
+    local label="${entry/::*/}"
+    local cipher_text="${entry/*::/}"
+
+    decrypt_using_key "${key}" "${cipher_text}"
+}
+
+function retrieve_pkey()
+{
+    local file="$1"
+
+    local first_line="$(sed -n '1{p;q}' "${file}")"
+
+    retrieve_pkey_from_line  "${first_line}"
+}
+
+function retrieve_pkey_from_line()
+{
+    local line="${1}"
+
+    local salt="${line:0:32}"
+    local entry="${line:32}"
+
+    decrypt_entry "${salt}" "${entry}" | xxd -p  | mk_one_line
+}
+
+case "${mode}" in
+    s )
+        ;&
     store )
         store
             ;;
+    r )
+        ;&
     retrieve )
         retrieve
         ;;
     dummy )
         dummy
         ;;
-    * ) 
+    cpw )
+        ;&
+    change_password )
+        change_password
+        ;;
+    * )
         usage
         ;;
 esac
